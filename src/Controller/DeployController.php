9928b99d25987125bde3ccb6697cec4b86dddc44
lucien burdet <lucien.burdet@edf.fr>
2020-10-27 09:19:29 +0100

    deploy script

---
+++ src/Controller/DeployController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class DeployController extends AbstractController
+{
+    /**
+     * @Route("/deploy", name="deploy")
+     */
+    public function index()
+    {
+        $commands = array(
+            'echo $PWD',
+            'whoami',
+            'git reset --hard HEAD',
+            'git pull',
+            'git status',
+            'git submodule sync',
+            'git submodule update',
+            'git submodule status',
+            'php /bin/console cache:clear --no-warmup --env=prod',
+        );
+        
+        $i = 0;
+        $output = [];
+        foreach($commands as $command){
+            /* Run command */
+            $tmp = shell_exec($command);
+            /* Output */
+            $output[$i]['command'] = $command; 
+            $output[$i]['result'] = htmlentities(trim($tmp));
+        }
+
+        return $this->json([
+            $output
+        ]);
+    }
+}
