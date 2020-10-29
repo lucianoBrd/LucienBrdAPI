91acd08c5f6fcb59f8be5056ba955e5cb13e49b6
lucianoBrd <lucien.burdet@gmail.com>
2020-10-29 10:53:51 +0100

    optimize services

--- src/Service/DatabaseGenerator.php
+++ src/Service/DatabaseGenerator.php
@@ -115,12 +115,12 @@ class DatabaseGenerator
         $this->reset($repository);
 
         $services = [
-            ['web.png', 'Web', 'Je peux développer tous types de site : E-commerce, site vitrine, blog...'],
-            ['communication.png', 'Communication', 'Vous ne savez pas comment bien utiliser les réseaux sociaux ? Je peux gérer vos comptes afin d’accroître votre notoriété.'],
-            ['publicite.png', 'Publicité', 'Afin de d’augmenter votre présence sur internet il vous faut les meilleures publicités : image, bannière, vidéo...'],
-            ['web-design.png', 'Web design', 'Je respecte les derniers standards et normes de design afin de vous proposer un produit numérique beau et fonctionnel.'],
-            ['logo.png', 'Logo', 'Création de logo sur mesure avec une identité forte et de qualité.'],
-            ['referencement.png', 'Référencement', 'Je peux positionner les pages web de votre site internet dans les premiers résultats naturels des moteurs de recherche.'],
+            ['web.webp', 'Web', 'Je peux développer tous types de site : E-commerce, site vitrine, blog...'],
+            ['communication.webp', 'Communication', 'Vous ne savez pas comment bien utiliser les réseaux sociaux ? Je peux gérer vos comptes afin d’accroître votre notoriété.'],
+            ['publicite.webp', 'Publicité', 'Afin de d’augmenter votre présence sur internet il vous faut les meilleures publicités : image, bannière, vidéo...'],
+            ['web-design.webp', 'Web design', 'Je respecte les derniers standards et normes de design afin de vous proposer un produit numérique beau et fonctionnel.'],
+            ['logo.webp', 'Logo', 'Création de logo sur mesure avec une identité forte et de qualité.'],
+            ['referencement.webp', 'Référencement', 'Je peux positionner les pages web de votre site internet dans les premiers résultats naturels des moteurs de recherche.'],
         ];
 
         foreach ($services as $s) {
