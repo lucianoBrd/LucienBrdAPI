390277d81ed27c39d0f7770091e0a663d97833fe
lucianoBrd <lucien.burdet@gmail.com>
2020-10-30 10:50:30 +0100

    review mail + security contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Component\Mime\Email;
+use Symfony\Component\Mime\Address;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -17,18 +18,24 @@ class ContactController extends AbstractController
     {
         $error = false;
 
-        $email = (new Email())
-            ->from('no-reply@lucien-brd.com')
-            ->to('contact@lucien-brd.com')
-            ->priority(Email::PRIORITY_HIGH)
-            ->subject('Message de ' . $name)
-            ->html('<h1>' . $name . '</h1>')
-            ->html('<a href="mailto:' . $email . '">' . $email . '</a>')
-            ->html('<p>' . $message . '</p>');
+        if ($name && !empty($name) && $email && !empty($email) && $message && !empty($message)) {
+            $html = '<h1>' . $name . '</h1>';
+            $html .= '<a href="mailto:' . $email . '">' . $email . '</a>';
+            $html .= '<p>' . $message . '</p>';
+            $html .= fopen('../public/assets/email/mail.html', 'r');
 
-        try {
-            $mailer->send($email);
-        } catch (TransportExceptionInterface $e) {
+            $email = (new Email())
+                ->from(Address::fromString('Lucien Burdet <no-reply@lucien-brd.com>'))
+                ->to('contact@lucien-brd.com')
+                ->subject('Message de ' . $name)
+                ->html($html);
+
+            try {
+                $mailer->send($email);
+            } catch (TransportExceptionInterface $e) {
+                $error = true;
+            }
+        } else {
             $error = true;
         }
 
