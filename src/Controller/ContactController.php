e1208a5b8ecd803351e78a1f6d94e9bd0fd8c21f
lucianoBrd <lucien.burdet@gmail.com>
2020-10-30 18:51:40 +0100

    get json contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Address;
@@ -12,66 +13,71 @@ use Symfony\Component\Routing\Annotation\Route;
 class ContactController extends AbstractController
 {
     /**
-     * @Route("/contact/{captcha}/{name}/{mail}/{message}", name="contact")
+     * @Route("/contact", name="contact")
      */
-    public function index(MailerInterface $mailer, string $captcha, string $name, string $mail, string $message)
+    public function index(MailerInterface $mailer, Request $request)
     {
-        $error = false;
+        $error = true;
 
-        /* Remove all illegal characters from mail */
-        $mail = filter_var($mail, FILTER_SANITIZE_EMAIL);
+        if ($request->getMethod() == 'POST') {
+            $postData = $request->request;
+            if ($postData->has('captcha') && $postData->has('name') && $postData->has('mail') && $postData->has('message')) {
+                $captcha = htmlspecialchars($postData->get('captcha'));
+                $name = htmlspecialchars($postData->get('name'));
+                $mail = htmlspecialchars($postData->get('mail'));
+                $message = htmlspecialchars($postData->get('message'));
 
-        /* Check captcha */
-        $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret=' . $_ENV['PRIVATE_KEY'] . '&response=' . $captcha);
-        $resp = json_decode($verifyResponse);
+                /* Remove all illegal characters from mail */
+                $mail = filter_var($mail, FILTER_SANITIZE_EMAIL);
 
-        if (!($resp != null && $resp->success)) {
-            $error = true;
-        }
+                /* Check captcha */
+                $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret=' . $_ENV['PRIVATE_KEY'] . '&response=' . $captcha);
+                $resp = json_decode($verifyResponse);
 
-        if (!$error &&
-            $name && !empty($name) &&
-            $mail && !empty($mail) && filter_var($mail, FILTER_VALIDATE_EMAIL) &&
-            $message && !empty($message)
-        ) {
-            $html = '<h2>' . $name . '</h2>';
-            $html .= '<a href="mailto:' . $mail . '">' . $mail . '</a>';
-            $html .= '<p>' . $message . '</p>';
+                if ($resp != null && $resp->success &&
+                    $name && !empty($name) &&
+                    $mail && !empty($mail) && filter_var($mail, FILTER_VALIDATE_EMAIL) &&
+                    $message && !empty($message)
+                ) {
+                    $html = '<h2>' . $name . '</h2>';
+                    $html .= '<a href="mailto:' . $mail . '">' . $mail . '</a>';
+                    $html .= '<p>' . $message . '</p>';
 
-            /* Get signature */
-            $signature = '';
-            $f = fopen('../public/assets/email/mail.html', 'r');
-            while (!feof($f)) {
-                $result = fgets($f);
-                $signature .= $result;
+                    /* Get signature */
+                    $signature = '';
+                    $f = fopen('../public/assets/email/mail.html', 'r');
+                    while (!feof($f)) {
+                        $result = fgets($f);
+                        $signature .= $result;
+                    }
+                    fclose($f);
+
+                    $html .= $signature;
+
+                    $confirm = '<h1>Bonjour ' . $name . '</h1>';
+                    $confirm .= '<p>Merci pour votre message, je vous répondrais dans les meilleurs délais.</p>';
+                    $confirm .= '<p>---------------------------------------</p>';
+
+                    $email = (new Email())
+                        ->from(Address::fromString('Lucien Burdet <no-reply@lucien-brd.com>'))
+                        ->to('lucien.burdet@gmail.com')
+                        ->subject('Message de ' . $name)
+                        ->html($html);
+
+                    $emailConfirm = (new Email())
+                        ->from(Address::fromString('Lucien Burdet <no-reply@lucien-brd.com>'))
+                        ->to(new Address($mail))
+                        ->subject('Confirmation de récéption')
+                        ->html($confirm . $html);
+                    try {
+                        $mailer->send($email);
+                        $mailer->send($emailConfirm);
+                        $error = false;
+                    } catch (TransportExceptionInterface $e) {
+                        $error = true;
+                    }
+                }
             }
-            fclose($f);
-
-            $html .= $signature;
-
-            $confirm = '<h1>Bonjour ' . $name . '</h1>';
-            $confirm .= '<p>Merci pour votre message, je vous répondrais dans les meilleurs délais.</p>';
-            $confirm .= '<p>---------------------------------------</p>';
-
-            $email = (new Email())
-                ->from(Address::fromString('Lucien Burdet <no-reply@lucien-brd.com>'))
-                ->to('lucien.burdet@gmail.com')
-                ->subject('Message de ' . $name)
-                ->html($html);
-
-            $emailConfirm = (new Email())
-                ->from(Address::fromString('Lucien Burdet <no-reply@lucien-brd.com>'))
-                ->to(new Address($mail))
-                ->subject('Confirmation de récéption')
-                ->html($confirm . $html);
-            try {
-                $mailer->send($email);
-                $mailer->send($emailConfirm);
-            } catch (TransportExceptionInterface $e) {
-                $error = true;
-            }
-        } else {
-            $error = true;
         }
 
         return $this->json([
