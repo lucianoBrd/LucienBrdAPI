f0ce1c3312e4b22899e0c67fd14c0066099a1f6a
lucianoBrd <lucien.burdet@gmail.com>
2020-11-20 14:56:28 +0100

    avoid bug ovh

--- src/Controller/MailController.php
+++ src/Controller/MailController.php
@@ -48,7 +48,7 @@ class MailController extends AbstractController
 
         $message = (new \Swift_Message($title))
             ->setFrom('lucien.burdet@gmail.com')
-            ->setTo('contact@lucien-brd.com')
+            ->setTo('lucien.burdet@gmail.com')
             ->setBody($m, 'text/html');
 
         try {
